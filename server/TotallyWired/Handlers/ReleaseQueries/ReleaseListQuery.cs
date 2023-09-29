e0a68b22cfb8e1528036664f1fa3ac246c83a44d
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2023-09-30 10:53:55 +1300

    Supoort country & label release filtering

--- server/TotallyWired/Handlers/ReleaseQueries/ReleaseListQuery.cs
+++ server/TotallyWired/Handlers/ReleaseQueries/ReleaseListQuery.cs
@@ -10,6 +10,8 @@ public class ReleaseListSearchParams
 {
     public string? Q { get; set; }
     public int? Year { get; set; }
+    public string? Label { get; set; }
+    public string? Country { get; set; }
 }
 
 public class ReleaseListQueryHandler
@@ -31,6 +33,8 @@ public class ReleaseListQueryHandler
     {
         var userId = _user.UserId();
         var year = @params.Year;
+        var label = @params.Label;
+        var country = @params.Country;
         var tsQuery = @params.Q.TsQuery();
         var hasQuery = tsQuery.Length >= 3;
 
@@ -43,6 +47,14 @@ public class ReleaseListQueryHandler
         if (@params.Year.HasValue)
         {
             query = query.Where(r => r.Year == year);
+        }
+        if (!string.IsNullOrEmpty(@params.Label))
+        {
+            query = query.Where(r => r.RecordLabel == label);
+        }
+        if (!string.IsNullOrEmpty(@params.Country))
+        {
+            query = query.Where(r => r.Country == country);
         }
 
         var releases = await query
